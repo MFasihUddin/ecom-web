e98c4d287e350c199e3600086c24a5a8833268d3
fasih <fasih0070@gmail.com>
2023-09-02 10:29:05 +0500

    nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
-import { FiShoppingCart } from 'react-icons/fi';
+import { FiShoppingCart } from "react-icons/fi";
+import { CgMenu, CgClose } from "react-icons/cg";
 
 function Nav() {
+  const [menuIcon, setMenuIcon] = useState();
+  const handleClick = () => {
+    setMenuIcon(false);
+  };
   const Nav = styled.nav`
     .navbar-lists {
       display: flex;
@@ -159,40 +164,64 @@ function Nav() {
   `;
   return (
     <Nav>
-      <div className="navbar">
+      <div className={menuIcon ? "navbar active" : "navbar"}>
         <ul className="navbar-lists">
           <li>
-            <NavLink to="/" className="navbar-link home-link">
+            <NavLink to="/" className="navbar-link" onClick={handleClick}>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to="/about" className="navbar-link">
+            <NavLink to="/about" className="navbar-link" onClick={handleClick}>
               About
             </NavLink>
           </li>
           <li>
-            <NavLink to="/products" className="navbar-link">
+            <NavLink
+              to="/products"
+              className="navbar-link"
+              onClick={() => setMenuIcon(false)}
+            >
               Products
             </NavLink>
           </li>
           <li>
-            <NavLink to="/contact" className="navbar-link">
+            <NavLink
+              to="/contact"
+              className="navbar-link"
+              onClick={handleClick}
+            >
               Contact
             </NavLink>
           </li>
           <li>
-            <NavLink to="" className="navbar-link">
+            <NavLink to="" className="navbar-link" onClick={handleClick}>
               Login
             </NavLink>
           </li>
           <li>
-            <NavLink to="/cart" className="navbar-link cart-trolley--link">
-              <FiShoppingCart className="cart-trolley"/>
+            <NavLink
+              to="/cart"
+              className="navbar-link cart-trolley--link"
+              onClick={handleClick}
+            >
+              <FiShoppingCart className="cart-trolley" />
               <span className="cart-total--item">10</span>
             </NavLink>
           </li>
         </ul>
+        <div className="mobile-navbar-btn">
+          <CgMenu
+            name="menu-outline"
+            className="mobile-nav-icon"
+            onClick={() => setMenuIcon(true)}
+          />
+          <CgClose
+            name="close-outline"
+            className="mobile-nav-icon close-outline"
+            onClick={handleClick}
+          />
+        </div>
       </div>
     </Nav>
   );
