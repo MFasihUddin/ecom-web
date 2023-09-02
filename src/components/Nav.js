d95f7841e8f203eab458ac548457316ab3027f60
fasih <fasih0070@gmail.com>
2023-09-02 09:40:20 +0500

    Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
+import { FiShoppingCart } from 'react-icons/fi';
 
 function Nav() {
   const Nav = styled.nav`
@@ -163,6 +164,32 @@ function Nav() {
           <li>
             <NavLink to="/" className="navbar-link home-link">
               Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className="navbar-link">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/products" className="navbar-link">
+              Products
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" className="navbar-link">
+              Contact
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="" className="navbar-link">
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/cart" className="navbar-link cart-trolley--link">
+              <FiShoppingCart className="cart-trolley"/>
+              <span className="cart-total--item">10</span>
             </NavLink>
           </li>
         </ul>
